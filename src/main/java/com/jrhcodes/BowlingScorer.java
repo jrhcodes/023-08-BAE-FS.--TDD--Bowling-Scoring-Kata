0dd8d5c3916549cd19559c747f7e95ea3f9d3d91
Julian <mrjrhirst@gmail.com>
2023-09-04 00:08:19 +0100

    Basic tests for values X X X X X X X X X X X X, 300 9- 9- 9- 9- 9- 9- 9- 9- 9- 9-, 90 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5, 150 in testScoreBowlingVsExpectedScores pasing

--- src/main/java/com/jrhcodes/BowlingScorer.java
+++ src/main/java/com/jrhcodes/BowlingScorer.java
@@ -2,45 +2,48 @@ package com.jrhcodes;
 
 public class BowlingScorer {
 
-    private static int digitToInt(char digit) { return digit - '0';}
+    private static int bowlScore(char digit) { return  (digit=='-') ? 0 : digit - '0';}
+    private static char getBowl(String frame, int index) { return frame.charAt(index);}
+
+
     public static int scoreBlowing(String scoresheet) {
-        String[] frames= scoresheet.split("");
 
-      int returnValue = 0;
+        String[] frames= scoresheet.split(" ");
 
-      for(int frame = 0; frame < 10; frame ++ ) {
-          String thisFrame = frames[frame];
-          if (thisFrame.equals("X")) {
-              final char nextBowl = frames[frame + 1].charAt(0);
-              if (nextBowl == 'X') {
-                  final char bowlAfterThat = frames[frame + 2].charAt(0);
-                  if (bowlAfterThat == 'X') {
-                      returnValue += 30;
-                  } else {
-                      returnValue += 20 + digitToInt(bowlAfterThat);
-                  }
-              } else {
-                  final char bowlAfterThat = frames[frame + 1].charAt(1);
-                  if (bowlAfterThat == '/') {
-                      returnValue += 20;
-                  } else {
-                      returnValue += 10 + digitToInt(nextBowl) + digitToInt(bowlAfterThat);
-                  }
-              }
-          } else if (thisFrame.charAt(1) == '/') { // spare
-              final char nextBowl = frames[frame + 1].charAt(0);
-              if (nextBowl == 'X') {
-                  returnValue += 20;
-              } else {
-                  returnValue += 10 + digitToInt(nextBowl);
-              }
-          } else {
+        int returnValue = 0;
 
-              returnValue += digitToInt(thisFrame.charAt(0)) + digitToInt(thisFrame.charAt(1));
-          }
-      }
+        for(int frame = 0; frame < 10; frame ++ ) {
+            String thisFrame = frames[frame];
+            if (thisFrame.equals("X")) {
+                final char nextBowl = getBowl(frames[frame + 1], 0);
+                if (nextBowl == 'X') {
+                    final char bowlAfterThat = getBowl(frames[frame + 2], 0);
+                    if (bowlAfterThat == 'X') {
+                        returnValue += 30;
+                    } else {
+                        returnValue += 20 + bowlScore(bowlAfterThat);
+                    }
+                } else {
+                    final char bowlAfterThat = getBowl(frames[frame + 1], 0);
+                    if (bowlAfterThat == '/') {
+                        returnValue += 20;
+                    } else {
+                        returnValue += 10 + bowlScore(nextBowl) + bowlScore(bowlAfterThat);
+                    }
+                }
+            } else if (getBowl(thisFrame, 1) == '/') { // spare
+                final char nextBowl = getBowl(frames[frame + 1], 0);
+                if (nextBowl == 'X') {
+                    returnValue += 20;
+                } else {
+                    returnValue += 10 + bowlScore(nextBowl);
+                }
+            } else {
+                returnValue += bowlScore(getBowl(thisFrame, 0)) + bowlScore(getBowl(thisFrame, 1));
+            }
+        }
 
-      return returnValue;
+        return returnValue;
     }
 
 }
--- src/test/java/com/jrhcodes/BowlingScorerTest.java
+++ src/test/java/com/jrhcodes/BowlingScorerTest.java
@@ -1,15 +1,20 @@
 package com.jrhcodes;
 
+import com.jrhcodes.BowlingScorer;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
-class BowlingScorerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BowlingScorerTest {
+
 
     @ParameterizedTest
     @CsvFileSource(resources = "scoresheetExpected.csv")
-    public static void testScoreBowlingVsExpectedScores( String scoresheet, int expectedScore ) {
+    public void testScoreBowlingVsExpectedScores(String scoresheet, int expectedScore) {
         assertEquals(BowlingScorer.scoreBlowing(scoresheet), expectedScore);
     }
 }
