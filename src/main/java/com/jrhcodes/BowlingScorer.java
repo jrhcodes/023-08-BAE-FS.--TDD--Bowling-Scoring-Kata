e9b9ab696f6a1d4bed75e483b058c1a89c8acf83
Julian <mrjrhirst@gmail.com>
2023-09-04 00:41:01 +0100

    Relocated test file to suit this project.

--- src/main/java/com/jrhcodes/BowlingScorer.java
+++ src/main/java/com/jrhcodes/BowlingScorer.java
@@ -2,24 +2,37 @@ package com.jrhcodes;
 
 public class BowlingScorer {
 
-    private static int bowlScore(char digit) { return  (digit=='-') ? 0 : digit - '0';}
-    private static char getBowl(String frame, int index) { return frame.charAt(index);}
+    private static int bowlScore(char digit) {
+        return (digit == '-') ? 0 : digit - '0';
+    }
 
-    private static boolean isStrike( String frame) {return frame.equals("X");};
-    private static boolean isStrike( char bowl ) {return bowl == 'X';};
-    private static boolean isSpare( char bowl ) {return bowl == '/';};
+    private static char getBowl(String frame, int index) {
+        return frame.charAt(index);
+    }
+
+    private static boolean isStrike(String frame) {
+        return frame.charAt(0) == 'X';
+    }
+
+    private static boolean isStrike(char bowl) {
+        return bowl == 'X';
+    }
+
+    private static boolean isSpare(char bowl) {
+        return bowl == '/';
+    }
 
     public static int scoreBlowing(String scoresheet) {
 
-        String[] frames= scoresheet.split(" ");
+        String[] frames = scoresheet.split(" ");
 
         int returnValue = 0;
 
-        for(int frame = 0; frame < 10; frame ++ ) {
+        for (int frame = 0; frame < 10; frame++) {
             String thisFrame = frames[frame];
             if (isStrike(thisFrame)) {
                 final char nextBowl = getBowl(frames[frame + 1], 0);
-                if (isStrike(nextBowl) ) {
+                if (isStrike(nextBowl)) {
                     final char bowlAfterThat = getBowl(frames[frame + 2], 0);
                     if (isStrike(bowlAfterThat)) {
                         returnValue += 30;
--- src/test/java/com/jrhcodes/BowlingScorerTest.java
+++ src/test/java/com/jrhcodes/BowlingScorerTest.java
@@ -1,16 +1,11 @@
 package com.jrhcodes;
 
-import com.jrhcodes.BowlingScorer;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingScorerTest {
-
 
     @ParameterizedTest
     @CsvFileSource(resources = "scoresheetExpected.csv")
